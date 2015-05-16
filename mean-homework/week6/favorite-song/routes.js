71245f77defbaa13aa780c6197db96e280c4567c
509Dave16 <davidfall@rocketmail.com>
2015-05-16 08:20:54 -0700

    Implemented the /tracks POST route  and the stubbed out /newest GET route.

--- mean-homework/week6/favorite-song/routes.js
+++ mean-homework/week6/favorite-song/routes.js
@@ -5,8 +5,8 @@ var router = express.Router();
 module.exports = exports = router;
 
 
-router.get('/', function (req, res) {  
-  res.render('home', {title: 'Page Title'});
+router.get('/home', function (req, res) {  
+  res.render('home', {title: "What's your favorite song?"});
 });
 
 router.get('/popular', function (req, res) {  
@@ -14,7 +14,7 @@ router.get('/popular', function (req, res) {
 });
 
 router.get('/newest', function (req, res) {  
-  
+  res.send('Newest Routes');
 });
 
 router.get('/random', function (req, res) {  
@@ -22,5 +22,38 @@ router.get('/random', function (req, res) {
 });
 
 router.post('/tracks', function (req, res){
-	
+	console.log(req.body);
+	var name = req.body.track_name;
+	var artist = req.body.artist_name;
+	console.log(name);
+	console.log(artist);
+	var where = {};
+	where.name = name;
+	where.artist = artist;
+
+	FavoriteSong.findOne(where,
+		function(error, favoriteSong)
+		{
+			if(!favoriteSong)
+			{
+				favoriteSong = new FavoriteSong();
+				favoriteSong.popularity = 0;
+			}
+			favoriteSong.name = name;
+			favoriteSong.artist = artist;
+			favoriteSong.popularity++;
+			favoriteSong.timestamp = new Date();
+			favoriteSong.save(
+				function(error,savedFavoriteSong)
+				{
+					if(savedFavoriteSong)
+					{
+						console.log(savedFavoriteSong);
+						// res.render('',{});
+						res.send(savedFavoriteSong);
+					}
+				}
+			);
+		}
+	);
 });
