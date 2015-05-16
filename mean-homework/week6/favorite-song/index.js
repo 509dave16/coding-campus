b8763fe3b91c82bfc33ec3e5a60f9997a3d0bfc3
509Dave16 <davidfall@rocketmail.com>
2015-05-16 08:19:30 -0700

    Added body parsing middleware registration and modified base route.

--- mean-homework/week6/favorite-song/index.js
+++ mean-homework/week6/favorite-song/index.js
@@ -5,15 +5,13 @@ var handlebars = require('express-handlebars');
 var routes = require('./routes.js');
 var PORT = PORT || 8080;
 
-
 app.use(express.static(__dirname + '/www'));  
 app.engine('handlebars', handlebars({defaultLayout: 'main'}));  
 app.set('view engine', 'handlebars'); 
-app.use('/favorite-song/',routes);
 
-app.get('/', function (req, res) {  
-  res.render('home', {title: 'Page Title'});
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use('/',routes);
 
 app.listen(PORT);
 console.log("Listening on Port: " + PORT);
